19f13c5d00e8517bf4d35502689f51dc0d9443f8
venkatesh669 <pillarivenkatesh996@gmail.com>
2025-08-21 16:14:20 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,22 +1,13 @@
 const request = require("supertest");
 const app = require("../app");
 
-let server;
-
-beforeAll((done) => {
-  server = app.listen(3000, done); // Start server before tests
-});
-
-afterAll((done) => {
-  server.close(done); // Close server after tests
-});
-
 describe("GET /", () => {
   it("should return Hello message", async () => {
-    const res = await request(server).get("/");
+    const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe("Hello from CI/CD with GitHub Actions & Docker!🚀");
   });
 });
+
 
  
