0ae2de6c0ef596097315367c7f89890a4a6f970e
venkatesh669 <pillarivenkatesh996@gmail.com>
2025-08-21 15:19:14 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD with GitHub Actions & Docker! 🚀');
+  res.send('Hello from CI/CD with GitHub Actions & Docker!🚀');
 });
 
 // Only start server if this file is run directly (not during tests)
