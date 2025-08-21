582c4cf7a4abbbe511b6af6f39663367af81dbe1
root <root@ip-172-31-46-23.eu-north-1.compute.internal>
2025-08-21 09:44:46 +0000

    app.json commits

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const PORT = 3000;
 
 app.get("/", (req, res) => res.send("Hello from CI/CD with GitHub Actions & Docker! 🚀"));
 
-app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`App running on port ${PORT}`));
+
 
 module.exports = app;
 
