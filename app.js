fa42d6378b12b0c759b36f704eeebb2f6fe1e775
venkatesh669 <pillarivenkatesh996@gmail.com>
2025-08-21 16:01:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => res.send("Hello from CI/CD with GitHub Actions & Docker! 🚀"));
+app.get("/", (req, res) => res.send("Hello from CI/CD with GitHub Actions & Docker!🚀"));
 
 app.listen(PORT, '0.0.0.0', () => console.log(`App running on port ${PORT}`));
 
